4d4f013c083f0c9a9a6fe73ea829280aef942e40
Official-Krish <krishanand0103@gmail.com>
2024-12-07 17:42:58 +0530

    build fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), nodePolyfills()],
 })
